8a840b64efed3af767eb093bcb0062949cd12377
Austin Gerald <austinwgerald@yahoo.com>
2019-03-10 13:58:38 -0500

    Adding loading icon, updated dependencies, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dad-joke
 React &amp; Express app to get random Dad jokes courtesy of https://icanhazdadjoke.com/
+
+# Running on local
+```sh
+$ cd dad-joke
+$ npm run dev
+```
--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,61 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import './App.css';
-import Footer from './Components/Footer'
+import Footer from './Components/Footer';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+ 
+library.add(
+  fab,
+  faSpinner
+)
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       joke: '',
-      message: ''
+      message: '',
+      loading: false
     };
-  }
-
-  handleClick = () => {
-    this.getJokeData();
   }
 
   componentDidMount() {
     this.getJokeData();
   }
 
+  handleClick = () => {
+    this.getJokeData();
+  }
+
   getJokeData() {
     console.log('Getting a new serving of fresh, organic dad joke.');
-    this.callApi()
+
+    this.setState({ loading: true }, () => {
+      this.callApi()
       .then(res => this.validate(res))
       .catch(err => console.log(err));
+    });
+
   }
 
   callApi = async () => {
     const response = await fetch('/api/joke');
     const body = await response.json();
     console.log(body)
+
     if (response.status !== 200) throw Error(body.message);
+
     return body;
   }
 
   validate(res) {
+    //Reset loading
+    this.setState({ loading: false });
+
     if (res.status === 200 && res.joke !== null) {
       this.setState({ joke: res.joke });
     }
@@ -49,21 +69,31 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="App" >
         <header className="App-header">
           <p className="App-title">React(ing) to Dad Jokes.</p>
         </header>
+
         <div className="Message">
           {this.state.message}
         </div>
-        <Fade left spy={this.state.joke}>
-          <div className="Joke">
-            {this.state.joke.split("\n").map((i, key) => {
-              return <div key={key}>{i}</div>;
-            })}
-          </div>
-        </Fade>
+
+        { 
+          this.state.loading ?
+
+          <FontAwesomeIcon icon={['fas', 'spinner']} pulse size="4x" /> :
+
+          <Fade left spy={this.state.joke}>
+            <div className="Joke">
+              {this.state.joke.split("\n").map((i, key) => {
+                return <div key={key}>{i}</div>;
+              })}
+            </div>
+          </Fade>
+        }
+
         <br /><br />
+
         <button className="Button" onClick={this.handleClick}>
           Another!
         </button>
